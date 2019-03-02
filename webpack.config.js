52fd9e714ce280b9cc5eb135c5b8a92f6474bc2b
Maly Phongsavanh <phongsavanh619@icloud.com>
2019-03-01 17:02:03 -0800

    add multiple html files and routing to each

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
 
@@ -51,11 +52,54 @@ module.exports = {
   },
 
   plugins: [
+
     new HtmlWebpackPlugin({
       inject: 'body',
       template: './src/index.html',
       filename: 'index.html',
-    })
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true
+      }
+    }),
+
+    new HtmlWebpackPlugin({
+      template: './src/workspace.html',
+      filename: 'workspace.html',
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true
+      }
+    }),
+
+    new HtmlWebpackPlugin({
+      template: './src/amenities.html',
+      filename: 'amenities.html',
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true
+      }
+    }),
+
+    new HtmlWebpackPlugin({
+      template: './src/join.html',
+      filename: 'join.html',
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true
+      }
+    }),
+
+    new HtmlWebpackPlugin({
+      template: './src/contact.html',
+      filename: 'contact.html',
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true
+      }
+    }),
+
+    new UglifyJsPlugin(),
 
   ]
 
