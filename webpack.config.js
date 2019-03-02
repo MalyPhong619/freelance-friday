dd6b13050570e4b8d1b194b2eada11fd7cd55353
Maly Phongsavanh <phongsavanh619@icloud.com>
2019-03-01 17:05:20 -0800

    install clean webpack and clean up white space in webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,28 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
-
   entry: './src/index.js',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist')
   },
-
   devtool: 'eval-source-map',
   devServer: {
     contentBase: './dist'
   },
-
   module: {
     rules: [
-
       {
         test: /\.scss$/,
         use: [
-            "style-loader",
-            "css-loader",
-            "sass-loader"
+          "style-loader",
+          "css-loader",
+          "sass-loader"
         ]
       },
-
       {
         test: /\.(gif|png|jpe?g)$/,
         use: [
@@ -39,20 +35,16 @@ module.exports = {
           }
         ]
       },
-
       {
-         test: /\.html$/,
-         loader: 'html-srcsets-loader',
-         options: {
-             attrs: ['img:src', ':srcset'],
-         },
+        test: /\.html$/,
+        loader: 'html-srcsets-loader',
+        options: {
+          attrs: ['img:src', ':srcset'],
+        },
       }
-
     ]
   },
-
   plugins: [
-
     new HtmlWebpackPlugin({
       inject: 'body',
       template: './src/index.html',
@@ -62,7 +54,6 @@ module.exports = {
         collapseWhitespace: true
       }
     }),
-
     new HtmlWebpackPlugin({
       template: './src/workspace.html',
       filename: 'workspace.html',
@@ -71,7 +62,6 @@ module.exports = {
         collapseWhitespace: true
       }
     }),
-
     new HtmlWebpackPlugin({
       template: './src/amenities.html',
       filename: 'amenities.html',
@@ -80,7 +70,6 @@ module.exports = {
         collapseWhitespace: true
       }
     }),
-
     new HtmlWebpackPlugin({
       template: './src/join.html',
       filename: 'join.html',
@@ -89,7 +78,6 @@ module.exports = {
         collapseWhitespace: true
       }
     }),
-
     new HtmlWebpackPlugin({
       template: './src/contact.html',
       filename: 'contact.html',
@@ -98,9 +86,7 @@ module.exports = {
         collapseWhitespace: true
       }
     }),
-
     new UglifyJsPlugin(),
-
+    new CleanWebpackPlugin(['dist'])
   ]
-
 };
